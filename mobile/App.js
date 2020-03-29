3feb51363192375a32723ecc2652f9be0a0cdf39
Maveryk <pedrodutra1r@gmail.com>
2020-03-28 22:57:56 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes'
+
+// <div> <span> <p> <h1>
+// <header> <footer>
+
+ 
+export default function App() {
+  return (
+    <Routes />
+  );
+}
